c0263f9a73c207511ffc09c7d039db32b251f8db
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-10 19:18:57 -0300

    :sparkles: feat: add match finish match routes

--- app/backend/src/routes/Matche.routes.ts
+++ app/backend/src/routes/Matche.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import MatchController from '../controllers/Match.controller';
-// import tokenVerify from '../middlewares/auth';
+import tokenVerify from '../middlewares/auth';
 
 const matcheRouter = Router();
 
 matcheRouter
-  .get('/', MatchController.getAll);
-// .patch('/:id/finish',tokenVerify, MatchController.finishMatch)
+  .get('/', MatchController.getAll)
+  .patch('/:id/finish', tokenVerify, MatchController.finishMatch);
 // .patch('/:id', tokenVerify, MatchController.update)
 // .post('/', tokenVerify, MatchController.create);
 
