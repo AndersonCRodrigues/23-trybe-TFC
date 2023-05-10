dc93685c87371378f2dea4a7170fae90896c241e
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-10 17:35:19 -0300

    :sparkles: feat: add jwt auth function

---
+++ app/backend/src/utils/auth.ts
@@ -0,0 +1,41 @@
+import { sign, verify } from 'jsonwebtoken';
+
+const secretkey = process.env.JWT_SECRET || 'insecure';
+
+interface ITokenId {
+  id:number
+  message?: string
+}
+
+const createToken = (id: number) => {
+  const data = {
+    id,
+  };
+
+  const token = sign(
+    data,
+    secretkey,
+    {
+      expiresIn: '1d',
+      algorithm: 'HS256',
+    },
+  );
+
+  return token;
+};
+
+const decodeToken = (token: string) : ITokenId => {
+  const decode = verify(token, secretkey);
+  return decode as ITokenId;
+};
+
+const verifyToken = (token: string): ITokenId => {
+  try {
+    const validate = verify(token, secretkey);
+    return validate as ITokenId;
+  } catch (e) {
+    return { message: 'Token must be a valid token', id: 0 };
+  }
+};
+
+export { createToken, decodeToken, verifyToken };
