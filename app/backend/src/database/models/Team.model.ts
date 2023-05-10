0d4971b45c22b506bc5c3de78d92bd689b8042b4
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-10 17:19:19 -0300

    :sparkles: feat: add teams migration and model

---
+++ app/backend/src/database/models/Team.model.ts
@@ -0,0 +1,34 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from ".";
+
+export interface TeamAtrributes {
+  id: number;
+  teamName: string;
+}
+
+export type TeamCreateAttr = Omit<TeamAtrributes, 'id'>;
+
+class TeamModel extends Model<TeamAtrributes, TeamCreateAttr> {
+  declare id: number;
+  declare teamName: string;
+}
+
+TeamModel.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+}, {
+  tableName: 'teams',
+  sequelize,
+  timestamps: false,
+  underscored: true,
+});
+
+export default TeamModel;
