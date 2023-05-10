5a320825c1d12285c60e8032c4670549420fcc31
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-10 19:21:20 -0300

    add match controller update

--- app/backend/src/controllers/Match.controller.ts
+++ app/backend/src/controllers/Match.controller.ts
@@ -16,11 +16,11 @@ export default class MatchController {
     res.status(200).json({ message: 'Finished' });
   }
 
-  // public static async update(req: Request, res: Response) {
-  //   await MatchService.update(+req.params.id, req.body);
+  public static async update(req: Request, res: Response) {
+    await MatchService.update(+req.params.id, req.body);
 
-  //   res.status(200).json();
-  // }
+    res.status(200).json();
+  }
 
   // public static async create(req: Request, res: Response) {
   //   const result = await MatchService.create(req.body);
